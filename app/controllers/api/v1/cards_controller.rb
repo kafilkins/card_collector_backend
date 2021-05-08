6199660fefe035941cf2ae26d4b5f1939f1ede37
Keith Filkins <kafilkins@me.com>
2021-05-07 21:28:47 -0400

    Latest version.

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::CardsController < ApplicationController
 
     def create 
-        card = Card.new 
+        collection = Collection.find(params[:collection_id])
+        card = collection.cards.build({
+            player: 
+            description:
+        }) 
+        if card.save 
+            render json: card 
+        else
+            render json: {message: card.errors.messages}
+        end
     end
 
 end
