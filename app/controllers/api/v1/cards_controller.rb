571a60c9e531a8122b9a2ef36452eb850216c4cf
Keith Filkins <kafilkins@me.com>
2021-05-11 22:01:38 -0400

    Working on adding cards to my collections.

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -2,15 +2,15 @@ class Api::V1::CardsController < ApplicationController
 
     def create 
         collection = Collection.find(params[:collection_id])
-        card = collection.cards.build({
-            player: 
-            description:
-        }) 
-        if card.save 
-            render json: card 
-        else
-            render json: {message: card.errors.messages}
-        end
+       # card = collection.cards.build({
+       #     player: ,
+       #     description:
+        #}) 
+        #if card.save 
+        #    render json: card 
+        #else
+        #    render json: {message: card.errors.messages}
+        #end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :collections, only: [:index, :create, :destroy]
+      resources :cards, only: [:index, :create, :destroy]
     end
   end
 
