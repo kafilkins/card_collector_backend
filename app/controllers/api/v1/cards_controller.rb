5821f175080c60b14a82bc6e5635b28214ba971f
Keith Filkins <kafilkins@me.com>
2021-05-20 21:17:00 -0400

    Working on frontend.

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::CardsController < ApplicationController
 
     def create 
         byebug
-        collection = Collection.find(params[:collection_id])
-       # card = collection.cards.build({
-       #     player: ,
-       #     description:
+        #collection = Collection.find(params[:collection_id])
+        #card = collection.cards.build({
+        #    player: params[:player],
+        #    description: params[:description]
         #}) 
         #if card.save 
         #    render json: card 
